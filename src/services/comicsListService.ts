04d69a975cc94e7b78cedcfb3f5b9ae13e861c22
andrelcalado <andrelcalad@hotmail.com>
2022-06-27 19:03:13 -0300

    Security

--- src/services/comicsListService.ts
+++ src/services/comicsListService.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 import md5 from 'md5';
+import { publicKey, privateKey } from "./key";
 
-const publicKey = 'c7ff70ab30402dc4da556363c1324355';
-const privateKey = 'f34822eb99576b2fefbf610ed9621e52ea9751b2';
 const time = Number(new Date());
 const hash = md5(time + privateKey + publicKey);
 const baseURL = `http://gateway.marvel.com/v1/public/comics?orderBy=-modified&limit=100&ts=${time}&apikey=${publicKey}&hash=${hash}`;
