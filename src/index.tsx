4008d5a206dd0c643b0f70a68a240c9abb355b90
andrelcalado <andrelcalad@hotmail.com>
2022-06-27 17:18:24 -0300

    Ajustes Finos e UserContext

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { UserContextProvider } from "./providers/auth";
 import Home from "./pages/home";
-import { Provider } from "react-redux";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
